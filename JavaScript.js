d2ea1eaa8a2d6dcc59138d1f4dc913c54485e50e
Autu1995 <autu1995@outlook.com>
2017-11-26 11:29:05 +0800

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,9 @@
-
+// 打印对象
+var items = function (d) {
+    var description = ""
+    for (var i in d) {
+        var property = d[i]
+        description += i + " = " + property + "\n"
+    }
+    return description
+}
